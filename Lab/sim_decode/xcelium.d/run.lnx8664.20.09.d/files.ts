9926483eedf160043ae9245afb68e972d392a745
Admin <giahuy13032003@gmail.com>
2024-10-07 02:06:02 -0400

    Initial commit

---
+++ Lab/sim_decode/xcelium.d/run.lnx8664.20.09.d/files.ts
@@ -0,0 +1,14 @@
+1716966062 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/DecodeUnit.v
+1716966062 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/decode_if.sv
+1728280675 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/tb.sv
+1717419187 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/package_decode.sv
+1716966062 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/decode_table_package.sv
+1717416199 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/item.sv
+1717415139 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/gen_item_seq.sv
+1717416532 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/monitor.sv
+1717251482 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/driver.sv
+1716966062 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/agent.sv
+1717411541 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/scoreboard.sv
+1716966062 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/env.sv
+1716966062 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/base_test.sv
+1728279685 /home/admin/Desktop/Lab_Decode/Lab/src/boom_decode/test_case/test_TRUE_RANDOM.sv
